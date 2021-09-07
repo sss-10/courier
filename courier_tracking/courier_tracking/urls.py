e3d0f5bf9aaebd108278e85634df52599e42b602
sss-10 <sashankshekharshukla@gmail.com>
2021-09-08 01:01:22 +0530

    simple home page with nav bar

--- courier_tracking/courier_tracking/urls.py
+++ courier_tracking/courier_tracking/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from courier.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name="index"),
 ]
